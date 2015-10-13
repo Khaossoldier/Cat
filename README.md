7c402beac0158a00f14ebd492e9e56962882fc91
Nicolas Duflos <nicolas.duflos@orange.fr>
2015-10-13 15:12:50 +0200

    finalisation mcat-scd

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Cat
+# Commande Cat
+
+mcat-scd invoque la fonction cat avec différentes tailles de tampon afin de definir la taille optimale du tampon.
+
+Taille du tampon 		Facteur d'évaluation
+1						1
+2 						0.962
+4 						1
+8 						1
+16 						1
+32 						1
+64 						1
+128 					1
+256 					0.962
+512						0.962
+1024					1
+2048					1
+4096					1
+8192					1
+16384					1
+32768					0.962
+65536					1
+131072					0.96
+262144					1
+524288					0.962
+1048576					0.96
+2097152					0.962
+4194304					0.96
+8388608					0.962
--- mcat-scd.c
+++ mcat-scd.c
@@ -1,8 +1,27 @@
-/*fonction mcat*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#define MCAT_BUFSIZ 10
+
+/*fonction mcat-scd*/
 
 void
-mcat(char* path, int taille_tampon){
+mcat_scd(char* path){
 	int fd;
-	fd = open(*path, RD_ONLY);
-	while 
+	char buffer[MCAT_BUFSIZ];
+	fd = open(path, O_RDONLY);
+	while(read(fd, buffer, MCAT_BUFSIZ) != 0){
+		printf("%s", buffer);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2){
+		printf ("Vieux con \n");
+		return -1;
+	}
+	mcat_scd(argv[1]);
+	return 0;
 }
